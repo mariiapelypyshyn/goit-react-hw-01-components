8deeb3e839edeca4072eb3e6ae66a468363be5ed
mariiapelypyshyn <maria.peleshko97@gmail.com>
2023-10-16 14:55:22 +0300

    5

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -2,6 +2,7 @@ import css from './FriendList.module.css';
 import { FriendListItem } from '../FriendListItem/FriendListItem';
 
 export const FriendList = ({ friends }) => {
+    
     return (
         <ul className={css.friendList}>
             {friends.map(({ avatar, name, isOnline = true }) => (
